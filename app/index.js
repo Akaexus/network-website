ad3a80db86bccc1b89941d89de41321f1b1679fc
Dzban <dzban@protonmail.com>
2018-01-07 18:20:50 +0100

    init

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import css from 'semantic/semantic.less';
+import 'semantic/definitions/modules/transition';
+import 'semantic/definitions/modules/accordion';
+
+$('.ui.accordion').accordion();
+
+console.log('it\'s workin')
